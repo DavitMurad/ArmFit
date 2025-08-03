9d64daae781d1b869d38a8ad7c2f43b42f59ca15
“Davit <2840850M@student.gla.ac.uk>
2025-08-03 13:51:04 +0100

    Initial Commit

---
+++ ArmFit/ArmFit/ArmFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArmFitApp.swift
+//  ArmFit
+//
+//  Created by Davit Muradyan on 03.08.25.
+//
+
+import SwiftUI
+
+@main
+struct ArmFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
